6c5daddd5f465ae63d44771e8d1a29c236457c09
juanto77 <juanto77@gmail.com>
2022-01-20 11:41:42 -0600

    Added computerPlay selection & working on playRound function

---
+++ game.js
@@ -0,0 +1,14 @@
+// Computer input: Rock, Paper, Scissors
+function computerPlay() {
+    let random = ['rock', 'paper', 'scissors'];
+    return random[Math.floor(Math.random() * 3)];
+};
+console.log(Math.floor(Math.random() * 3));
+
+//User input: Rock, Paper, Scissors
+
+//Compare input & output winner
+function playRound(playerSelection, computerSelection) {
+    
+}
+//Score Keeper
