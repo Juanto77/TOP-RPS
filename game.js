2fc2b75478732b456ab1a2d6aa448b43300fe57e
Juanto77 <58269289+Juanto77@users.noreply.github.com>
2022-01-21 11:14:02 -0600

    Added a camparison function to declare winner, made the computerPlay more logical and cleaned up code. Working on declareWinner function to send a message to player.

--- game.js
+++ game.js
@@ -1,14 +1,83 @@
-// Computer input: Rock, Paper, Scissors
-function computerPlay() {
-    let random = ['rock', 'paper', 'scissors'];
-    return random[Math.floor(Math.random() * 3)];
-};
-console.log(Math.floor(Math.random() * 3));
+let roundWinner = '';
 
-//User input: Rock, Paper, Scissors
-
-//Compare input & output winner
+// Comparison Function
 function playRound(playerSelection, computerSelection) {
-    
+    if (
+        (playerSelection === 'ROCK' && computerSelection === 'ROCK') ||
+        (playerSelection === 'SCISSORS' && computerSelection === 'SCISSORS') ||
+        (playerSelection === 'PAPER' && computerSelection === 'PAPER')
+    ) {
+    roundWinner = 'Tie';
+    }
+    if (
+      (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+      (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
+      (playerSelection === 'PAPER' && computerSelection === 'ROCK')
+    ) {
+      roundWinner = 'Player';
+    }
+   if (
+      (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') ||
+      (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') ||
+      (playerSelection === 'ROCK' && computerSelection === 'PAPER')
+    ) {
+    roundWinner = 'Computer';
+    }
+
+  };
+
+// User Input v1
+function userPlay() {
+    let selection = prompt('Rock, Paper or Scissors?');
+    let finalSelection = selection.toUpperCase();
+    console.log(finalSelection);
+    return finalSelection;
+};
+
+
+// Computer input: Rock, Paper, Scissors v2
+
+function computerPlay() {
+    let array = [ 'Rock', 'Paper', 'Scissors']
+    let finalSelection = array[Math.floor(Math.random() * array.length)].toUpperCase();
+    return finalSelection
+};
+
+let playerSelection = userPlay();
+let computerSelection = computerPlay();
+
+//Declare a Winner using the roundWinner, playerSelecton & computerSelection.
+
+function declareWinner() {
+    if (roundWinner === 'Player') {
+        
+    }
 }
-//Score Keeper
+
+playRound(playerSelection, computerSelection);
+console.log(computerSelection);
+alert(`${roundWinner}`);
+
+/* Unused Code
+
+/* Computer input: Rock, Paper, Scissors v1
+function computerPlay() {
+    let random = ['Rock', 'Paper', 'Scissors'];
+    return random[Math.floor(Math.random()* random.length)].toUpperCase();
+};
+*/
+/*
+//Computer input v3
+function randomComputer() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return 'ROCK'
+        case 1: 
+            return 'PAPER'
+        case 2:
+            return 'SCISSORS'
+     
+    }
+};
+*/
